dbb042ddabb6aa41b3c3e105667f7fad20d9c18d
yamauch <yamanekosan.106@gmail.com>
2019-12-11 00:24:25 +0900

    画像のパスを引数に渡すとこまでできました

--- treehub/src/App.js
+++ treehub/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import image from './images/joucho_fuantei_man.png';
+//var Treelink = new Treelink[20];
+  var clicklink;
+function click_recieve(e) {
+  clicklink = image;
+  console.log(clicklink);
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={click_recieve} e = {image}>
+      <img src = {image} alt = ""/>
+      </button>
     </div>
   );
 }
