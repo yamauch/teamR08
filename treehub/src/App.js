4dbce24018f3d0ef6ec222e6676751b2d3e2c54b
yamauch <yamanekosan.106@gmail.com>
2019-12-13 01:12:07 +0900

    配列でも画像の受け渡しができるようになった

--- treehub/src/App.js
+++ treehub/src/App.js
@@ -4,29 +4,24 @@ import './App.css';
 import image from './images/joucho_fuantei_man.png';
 import image2 from './images/jk.png';
 import toumei from './images/toumei.png';
-//import { object } from 'prop-types';
-//var Treelink = new Treelink[20];
-
-
-
-
+var images = new Array(image,image2);
 function App() {
   const [image_state,getImage] = React.useState(toumei);
   const [send_image,setImage] = React.useState(toumei);
-  function click_recieve() {
-    console.log(image_state);
-    //({clicklink: image_url});  
-    getImage(image)
-    //console.log(image);
+  
+  function click_recieve(i) {
+    getImage(images[i]);
     console.log(image_state);
   }
+  
   function click_send(){
     setImage(image_state);
   }
+  
   return (
     <div className="App">
-      <button onClick={click_recieve}>
-        <img src={image} alt=""/>
+      <button onClick={ () =>click_recieve(1)} >
+        <img src={images[0]} alt=""/>
       </button>
       <button onClick={click_send}>
         <img src={send_image} alt=''/>
