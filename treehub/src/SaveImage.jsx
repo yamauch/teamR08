f1709459c9c35581ed9b26df2cae8fcecddecefa
shu-holly <shujp0101@gmail.com>
2019-12-14 01:53:51 +0900

    保持できるアイテムを増やした

--- treehub/src/SaveImage.jsx
+++ treehub/src/SaveImage.jsx
@@ -7,8 +7,15 @@ export default class SaveImage extends Component {
         super(props);
         this.state = {
             top: '',
-            middle: '',
-            bottom: '',
+            firstLeft: '',
+            firstRight: '',
+            secondLeft: '',
+            secondMiddle: '',
+            secondRight: '',
+            bottomLeft: '',
+            bottomMidLeft: '',
+            bottomMidRight: '',
+            bottomRight: '',
         };
         this.onClick = this.onClick.bind(this);
     }
@@ -16,14 +23,24 @@ export default class SaveImage extends Component {
     onClick() {
         firebaseDb.ref('decos').push({
             top: this.state.top,
-            middle: this.state.middle,
-            bottom: this.state.bottom,
+            firstLeft: this.state.firstLeft,
+            firstRight: this.state.firstRight,
+            secondLeft: this.state.secondLeft,
+            secondMiddle: this.state.secondMiddle,
+            secondRight: this.state.secondRight,
+            bottomLeft: this.state.bottomLeft,
+            bottomMidLeft: this.state.bottomMidLeft,
+            bottomMidRight: this.state.bottomMidRight,
+            bottomRight: this.state.bottomRight,
         });
-        this.setState({ top: '', middle: '', bottom: '' });
+        this.setState({ top: '', 
+                        firstLeft: '', firstRight: '',  
+                        secondLeft: '', secondMiddle: '', secondRight: '',
+                        bottomLeft: '', bottomMidLeft: '', bottomMidRight: '', bottomRight: '' });
     }
 
     render() {
-        const { top, middle, bottom } = this.state;
+        const { top, firstLeft, firstRight, secondLeft, secondMiddle, secondRight, bottomLeft, bottomMidLeft, bottomMidRight, bottomRight } = this.state;
         return (
             <div className="container">
                 <div className="field">
@@ -35,30 +52,86 @@ export default class SaveImage extends Component {
                     />
                 </div>
                 <div className="field">
-                    <label className="label">Middle</label>
+                    <label className="label">FirstLeft</label>
                     <input
                         className="input"
-                        value={this.state.middle}
-                        onChange={e => this.setState({ middle: e.target.value })}
+                        value={this.state.firstLeft}
+                        onChange={e => this.setState({ firstLeft: e.target.value })}
                     />
                 </div>
                 <div className="field">
-                    <label className="label">Bottom</label>
+                    <label className="label">FirstRight</label>
                     <input
                         className="input"
-                        value={this.state.bottom}
-                        onChange={e => this.setState({ bottom: e.target.value })}
+                        value={this.state.firstRight}
+                        onChange={e => this.setState({ firstRight: e.target.value })}
+                    />
+                </div>
+                <div className="field">
+                    <label className="label">SecondLeft</label>
+                    <input
+                        className="input"
+                        value={this.state.secondLeft}
+                        onChange={e => this.setState({ secondLeft: e.target.value })}
+                    />
+                </div>
+                <div className="field">
+                    <label className="label">SecondMiddle</label>
+                    <input
+                        className="input"
+                        value={this.state.secondMiddle}
+                        onChange={e => this.setState({ secondMiddle: e.target.value })}
+                    />
+                </div>
+                <div className="field">
+                    <label className="label">SecondRight</label>
+                    <input
+                        className="input"
+                        value={this.state.secondRight}
+                        onChange={e => this.setState({ secondRight: e.target.value })}
+                    />
+                </div>
+                <div className="field">
+                    <label className="label">BottomLeft</label>
+                    <input
+                        className="input"
+                        value={this.state.bottomLeft}
+                        onChange={e => this.setState({ bottomLeft: e.target.value })}
+                    />
+                </div>
+                <div className="field">
+                    <label className="label">BottomMidLeft</label>
+                    <input
+                        className="input"
+                        value={this.state.bottomMidLeft}
+                        onChange={e => this.setState({ bottomMidLeft: e.target.value })}
+                    />
+                </div>
+                <div className="field">
+                    <label className="label">bottomMidRight</label>
+                    <input
+                        className="input"
+                        value={this.state.bottomMidRight}
+                        onChange={e => this.setState({ bottomMidRight: e.target.value })}
+                    />
+                </div>
+                <div className="field">
+                    <label className="label">BottomRight</label>
+                    <input
+                        className="input"
+                        value={this.state.bottomRight}
+                        onChange={e => this.setState({ bottomRight: e.target.value })}
                     />
                 </div>
 
                 <div className="control">
-                    {top &&
+                    {/* {top &&
                         middle &&
-                            bottom && (
+                            bottom && ( */}
                                 <button className="button is-link" onClick={this.onClick}>
                                     Save
                                 </button>
-                            )}
+                            {/* )} */}
                 </div>
             </div>
         );
--- treehub/src/TreeDeco.js
+++ treehub/src/TreeDeco.js
@@ -10,10 +10,31 @@ export default class TreeDeco extends Component {
                         <p>{this.props.top}</p>
                     </div>
                     <div className="panel-block">
-                        <p>{this.props.middle}</p>
+                        <p>{this.props.firstLeft}</p>
                     </div>
                     <div className="panel-block">
-                        <p>{this.props.bottom}</p>
+                        <p>{this.props.firstRight}</p>
+                    </div>
+                    <div className="panel-block">
+                        <p>{this.props.secondLeft}</p>
+                    </div>
+                    <div className="panel-block">
+                        <p>{this.props.secondMiddle}</p>
+                    </div>
+                    <div className="panel-block">
+                        <p>{this.props.secondRight}</p>
+                    </div>
+                    <div className="panel-block">
+                        <p>{this.props.bottomLeft}</p>
+                    </div>
+                    <div className="panel-block">
+                        <p>{this.props.bottomMidLeft}</p>
+                    </div>
+                    <div className="panel-block">
+                        <p>{this.props.bottomMidRight}</p>
+                    </div>
+                    <div className="panel-block">
+                        <p>{this.props.bottomRight}</p>
                     </div>
                 </nav>
             </li>
