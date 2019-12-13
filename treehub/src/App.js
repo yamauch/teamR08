20f650a467cb380cbb5e46e9c239755e86e88353
shu-holly <shujp0101@gmail.com>
2019-12-14 00:23:16 +0900

    ルートコンポーネントを作成

--- treehub/src/App.js
+++ treehub/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TreeDecoList from './TreeDecoList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <section className="hero container is-info">
+          <div className="hero-body">
+            <h1 className="title">Treehub</h1>
+          </div>
+        </section>
+        <section className="container">
+          <TreeDecoList />
+        </section>
+      </div>
+    );
+  }
 }
-
-export default App;
